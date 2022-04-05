aa02d035348c2395e5be0a998f868d9f3a393929
Luis Fernando <lfmnovaes@gmail.com>
2022-04-05 14:03:21 -0300

    Hotfix

--- components/ProjectsNavbar.tsx
+++ components/ProjectsNavbar.tsx
@@ -22,7 +22,7 @@ const ProjectsNavbar: FunctionComponent<{
   return (
     <div className="flex px-3 py-2 space-x-3 overflow-x-auto list-none">
       <NavItem value="all" {...props} />
-      <NavItem value="node" {...props} />
+      <NavItem value="JS" {...props} />
       <NavItem value="react" {...props} />
       <NavItem value="nextjs" {...props} />
       <NavItem value="ruby on rails" {...props} />
