2788dadaebd79dfa3445a24290214488defe1093
Luis Fernando <lfmnovaes@gmail.com>
2022-04-05 16:24:20 -0300

    Close dialog when user clicks outside the project card

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,10 +1,27 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useCallback, useEffect, useRef } from 'react';
 import { AiFillGithub, AiFillProject } from 'react-icons/ai';
 import { MdClose } from 'react-icons/md';
 import { Project } from '../types';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
 import { fadeInUp, stagger } from '../animation';
+
+const handleClickOutside = (ref, handler) => {
+  useEffect(() => {
+    const listener = (event) => {
+      if (!ref.current || ref.current.contains(event.target)) {
+        return;
+      }
+      handler(event);
+    };
+    document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
+    return () => {
+      document.removeEventListener('mousedown', listener);
+      document.removeEventListener('touchstart', listener);
+    };
+  }, [ref, handler]);
+}
 
 const ProjectCard: FunctionComponent<{
   project: Project;
@@ -24,6 +41,10 @@ const ProjectCard: FunctionComponent<{
   showDetail,
   setShowDetail,
 }) => {
+  const divRef = useRef();
+  const handler = useCallback(() => setShowDetail(null), []);
+  handleClickOutside(divRef, handler);
+
   return (
     <div>
       <Image
@@ -32,12 +53,15 @@ const ProjectCard: FunctionComponent<{
         className="cursor-pointer"
         onClick={() => setShowDetail(id)}
         width="300"
-        height="150"
+        height="169"
         layout="responsive"
       />
       <p className="my-2 text-center">{name}</p>
       {showDetail === id && (
-        <div className="absolute top-0 left-0 z-10 grid w-full h-auto p-2 text-black bg-gray-100 rounded-lg md:grid-cols-2 gap-x-12 dark:text-white dark:bg-dark-100 md:p-10">
+        <div
+          ref={divRef}
+          className="absolute top-0 left-0 z-10 grid w-full h-auto p-2 text-black bg-gray-100 rounded-lg md:grid-cols-2 gap-x-12 dark:text-white dark:bg-dark-100 md:p-10"
+        >
           <motion.div variants={stagger} initial="initial" animate="animate">
             <motion.div
               variants={fadeInUp}
@@ -58,12 +82,18 @@ const ProjectCard: FunctionComponent<{
               <a
                 href={github_url}
                 className="flex items-center px-4 py-2 space-x-3 text-lg bg-gray-200 dark:bg-dark-200"
+                target="_blank"
+                rel="noreferrer"
+                aria-label="github project page"
               >
                 <AiFillGithub /> <span>Github</span>
               </a>
               <a
                 href={deployed_url}
                 className="flex items-center px-4 py-2 space-x-3 text-lg bg-gray-200 dark:bg-dark-200"
+                target="_blank"
+                rel="noreferrer"
+                aria-label="live demonstration"
               >
                 <AiFillProject /> <span>Live Demo</span>
               </a>
